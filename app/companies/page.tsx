d4fee9873c60924ec8490072e87c475f39c0c96f
timothée picard <timothee.picard@outlook.fr>
2025-03-17 14:55:36 +0100

    feat: list all company with real data and create new

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,23 +1,42 @@
+'use client'
 import CompanyCard from "@/components/company/CompanyCard";
+import { getAllCompanies } from "@/utils/fetcher/company";
+import {useEffect, useState} from "react";
+import LinksCompany from "@/components/company/Links";
 import ProtectedRoute from "@/components/auth/ProtectedRoute";
 
 export default function CompaniesPages() {
+	const [companies, setCompanies] = useState<[] | null>(null)
+	useEffect(() => {
+		const fetchCompanies = async () => {
+			try {
+				const data = await getAllCompanies();
+				setCompanies(data);
+			} catch (error) {
+				console.error("Erreur lors de la récupération des entreprises :", error);
+			}
+		};
+
+		fetchCompanies();
+	}, []);
+
 	return (
 		<ProtectedRoute>
-		<div>
-			<h1>Companies</h1>
+			<h1 className={'title'}>Companies</h1>
+			<LinksCompany />
 			<div className="flex flex-wrap gap-4 justify-between">
-				{
-					[...Array(18)].map((index) => (
+				{companies && companies.length > 0 ? (
+					companies.map((company: any) => (
 						<CompanyCard
-							key={index}
-							name={"Company Name"}
-							image={"https://cdn.futura-sciences.com/sources/images/AI-creation.jpg"}
+							key={company.id}
+							name={company.name}
+							image={company.image || "https://cdn.futura-sciences.com/sources/images/AI-creation.jpg"}
 						/>
 					))
-				}
+				) : (
+					<p>Aucune entreprise trouvée.</p>
+				)}
 			</div>
-		</div>
 		</ProtectedRoute>
-	)
+	);
 }
--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -6,9 +6,13 @@ interface TextInputProps {
     className?: string;
     placeholder?: string;
     value?: string;
+    id?: string;
+    name?: string
 }
 
-const TextInput: React.FC<TextInputProps>  = ({ onChange, label, className, placeholder }) => {
+const TextInput: React.FC<TextInputProps>  = (props) => {
+
+    const { onChange, label, className, placeholder, id, name } = props
     const [value, setValue] = React.useState<string | number>('');
 
     const handleChange= (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +26,8 @@ const TextInput: React.FC<TextInputProps>  = ({ onChange, label, className, plac
         <div className={'flex flex-col'}>
         <label className={'font-semibold mb-2 mt-2'}>{label}</label>
         <input
-            id="textInput"
+            id={id}
+            name={name}
             type="text"
             className={`text-primary font-semibold  bg-tertiary bg-opacity-40 py-1.5 px-4 rounded-[4px] ' ${className || ''}' placeholder:text-primary dark:text-white`}
             value={value}
